446330c0d25889c45b87f01a5c45be674e2805eb
Fatima Al-Hayajneh <2021980250@ses.yu.edu.jo>
2026-02-22 18:11:12 +0300

    feat: complete sqlite integration

--- main.py
+++ main.py
@@ -3,15 +3,17 @@ from models import Employee, Manager
 def main():
     try:
         name = input("Enter name: ")
-        emp_id = input("Enter ID: ")
+        emp_id = int(input("Enter ID: "))
         salary = float(input("Enter salary: "))
         dept = input("Enter department: ")
 
         mgr = Manager(name, emp_id, salary, dept)
         print(mgr.display_info())
         
-    except ValueError:
-        print("Error: Please enter a valid number for the salary.")
+        mgr.save_to_db()
 
-if __name__== "__main__":
+    except ValueError:
+        print("Error: Please enter valid numbers for ID and Salary.")
+
+if __name__ == "__main__":
     main()
